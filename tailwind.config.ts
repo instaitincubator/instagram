06bd694792a0748c6d1076be90aeee3637fbed2e
gem4ik <mgemskij@gmail.com>
2024-05-31 16:16:44 +0300

    KAN-3 init: start tailwind config settings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,14 +6,6 @@ const config: Config = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-      },
-    },
-  },
 }
 
 export default config
