b9e49ae614baeaa7c46476661fb6a7de308bc616
gem4ik <mgemskij@gmail.com>
2024-06-08 19:45:45 +0300

    KAN-4 feat: add Checkbox.tsx (ready for review)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import { getLayout } from '@/app/layouts/mainLayout/Layout'
 import { Checkbox } from '@/shared/ui/Checkbox/Checkbox'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className="h-[200px] ">
+    <div className="h-[200px] bg-dark-700">
       <div className="text-regular-link ">home</div>
-      <Checkbox checked className="pl-[400px]" label="checkbox" />
+      <Checkbox checked className="pl-[200px]" label="checkbox" />
     </div>
   )
 }
--- src/shared/ui/Checkbox/Checkbox.tsx
+++ src/shared/ui/Checkbox/Checkbox.tsx
@@ -14,26 +14,33 @@ export type CheckboxProps = ComponentPropsWithoutRef<'div'> &
 export const Checkbox = forwardRef<ElementRef<'div'>, CheckboxProps>(
   ({ checked, className, disabled = false, label, onValueChange, ...props }, ref) => {
     return (
-      <div className={clsx('flex select-none gap-2 items-center', className)} ref={ref} {...props}>
-        <div
-          className={clsx(
-            'w-[18px] relative h-[18px] border-[2px] border-light-100 rounded-sm ',
-            'before:h-[34px] before:w-[34px] before:hover:absolute before:active:bg-dark-100/30 before:hover:bg-dark-300/30 before:focus:bg-dark-500/30',
-            'before:rounded-3xl before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2 before:z-[-1]',
-            disabled && 'border-[2px] border-light-900',
-            checked && disabled && 'bg-light-900',
-            checked && 'bg-light-100'
-          )}
-          onClick={() => !disabled && onValueChange?.(!checked)}
-        >
-          {checked && (
-            <div className="flex items-center justify-center">
-              <Image alt="checked" height={24} src="/checkIcon.svg" width={24} />
-            </div>
-          )}
+      <div
+        className={clsx('flex select-none gap-2 items-center relative', className)}
+        ref={ref}
+        {...props}
+      >
+        <div className="relative group">
+          <div
+            className={clsx(
+              'w-[18px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 h-[18px] border-[2px] border-light-100 rounded-sm z-20',
+              disabled && 'border-[2px] border-light-900',
+              checked && disabled && 'bg-light-900',
+              checked && 'bg-light-100'
+            )}
+            onClick={() => !disabled && onValueChange?.(!checked)}
+          >
+            {checked && (
+              <div className="flex items-center justify-center">
+                <Image alt="checked" height={24} src="/checkIcon.svg" width={24} />
+              </div>
+            )}
+          </div>
+          <div className="z-10 absolute w-[34px] h-[34px] group-hover:bg-dark-300 group-active:bg-dark-100 group-focus:bg-dark-500  top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-3xl"></div>
         </div>
-        {label && <label className="text-regular-14 text-light-100 ">{label}</label>}
+        {label && <label className="text-regular-14 text-light-100 pl-[15px]">{label}</label>}
       </div>
     )
   }
 )
+// 'before:h-[34px] before:w-[34px] before:hover:absolute before:active:bg-dark-100/30 before:hover:bg-dark-300/30 before:focus:bg-dark-500/30',
+//   'before:rounded-3xl before:top-1/2 before:left-1/2 before:transform before:-translate-x-1/2 before:-translate-y-1/2',
