ac1620bc023c44f9989e18e3ef0db580bd76047c
hardmass <hardmass3ffect@gmail.com>
2024-06-09 16:07:14 +0300

    KAN-12 feat: add header, changed layout, add icons

---
+++ src/shared/ui/header/Header.tsx
@@ -0,0 +1,51 @@
+import { ComponentProps, useState } from 'react'
+
+import Button from '@/shared/ui/Button'
+import useIsMobile from '@/shared/ui/header/useIsMobile'
+import { Menu } from '@/shared/ui/icons/menu'
+import { Notification } from '@/shared/ui/icons/notification'
+import { clsx } from 'clsx'
+
+export type HeaderProps = {
+  isLoading?: boolean
+} & ComponentProps<'header'>
+
+export const Header = ({ className, isLoading, ...rest }: HeaderProps) => {
+  const breakPoint = 768
+  const isMobile = useIsMobile(breakPoint)
+  const [isAuth, setIsAuth] = useState(true)
+
+  return (
+    <header
+      {...rest}
+      className={clsx(
+        'flex pr-[5%] w-full h-[60px] justify-between bg-dark-700 text-light-100 items-center min-w-[360px] border-b border-b-dark-300',
+        className
+      )}
+    >
+      <span className="pl-[5%]">Instagram</span>
+      {isMobile && (
+        <div className="flex">
+          <span>сеелктор</span>
+          <Menu />
+        </div>
+      )}
+      {!isMobile && (
+        <div className="flex gap-[18px]">
+          <Notification />
+          <span>select</span>
+        </div>
+      )}
+      {!isAuth && (
+        <div className="flex">
+          <Button className="bg-blue-500 text-white rounded mr-4" size="m" variant="text">
+            Log in
+          </Button>
+          <Button className="bg-blue-500 text-white rounded" size="m">
+            Sign up
+          </Button>
+        </div>
+      )}
+    </header>
+  )
+}
