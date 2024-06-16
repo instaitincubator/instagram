e4c32b7dee5713f7bb0e262c1970e9cd86c34d8f
gem4ik <mgemskij@gmail.com>
2024-06-16 15:49:08 +0300

    KAN-23 chore: prepare store to work with requests

---
+++ src/app/inctagram-api.ts
@@ -0,0 +1,12 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+import config from '../../config'
+
+export const baseApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: config.baseUrl,
+    credentials: 'include',
+  }),
+  endpoints: () => ({}),
+  reducerPath: 'inctagram-api',
+})
