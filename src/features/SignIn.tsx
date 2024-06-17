96aa09adc312d42ee4ece381de9e0703e34b2b86
hardmass <hardmass3ffect@gmail.com>
2024-06-17 16:08:58 +0300

    KAN-15 refactor: refactor page

--- src/features/SignIn.tsx
+++ src/features/SignIn.tsx
@@ -8,10 +8,14 @@ export const SignInForm = () => {
 
   return (
     <form>
-      <Card className="flex flex-col p-[24px] items-center p-[24px]">
-        <span>Sign In</span>
-        <Image alt="git" height={36} src="/google.svg" width={36} />
-        <Image alt="git" height={36} src="/git.svg" width={36} />
+      <Card className="flex flex-col items-center">
+        <div className="py-[23px]">
+          <span>Sign In</span>
+        </div>
+        <div className="py-[13px]">
+          <Image alt="git" height={36} src="/google.svg" width={36} />
+          <Image alt="git" height={36} src="/git.svg" width={36} />
+        </div>
       </Card>
     </form>
   )
