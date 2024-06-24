e5332302d7327f29c37e4dff6c5271e6924f2d5c
ekaterinapelevina <katepelevina08@gmail.com>
2024-06-24 20:08:06 +0300

    KAN-27 fix:sign-up-form fix pull request comments

--- src/features/sign-up-form/useSignUpForm.ts
+++ src/features/sign-up-form/useSignUpForm.ts
@@ -4,20 +4,24 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 export interface SignUpFormType {
-  username: string
   email: string
   password: string
   passwordConfirmation: string
+  username: string
 }
 
 const schema = z.object({
+  accept: z.literal(true, {
+    invalid_type_error: 'You must accept Terms of Service and Privacy Policy',
+  }),
   email: z
     .string()
     .min(1, { message: 'email required' })
     .email({ message: 'The email must match the format example@example.com' }),
-  accept: z.literal(true, {
-    invalid_type_error: 'You must accept Terms of Service and Privacy Policy',
-  }),
+  username: z
+    .string()
+    .min(6, { message: 'The username must contain min 6 characters' })
+    .max(30, { message: 'The username must contain max 30 characters' }),
 })
 
 export const useSignUpForm = () => {
