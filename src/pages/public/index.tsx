fe8311e7ec7b6ed8c04e18bfc0181540d4fd0c9b
mikhailst1ff <104980055+mikhailst1ff@users.noreply.github.com>
2024-06-30 06:31:42 +0300

    add error

--- src/pages/public/index.tsx
+++ src/pages/public/index.tsx
@@ -1,16 +1,20 @@
+import React from 'react';
+
 import {getLayout} from '@/app/layouts/mainLayout/Layout';
 import CountRegisteredUsers from '@/entities/CountRegisteredUsers/CountRegisteredUsers';
 import PublicPosts from '@/entities/PublicPosts/PublicPosts';
 import {useGetAllPublicPostsQuery} from '@/features/public/allPublicPost';
 import {useGetTotalUsersCountQuery} from '@/features/public/publicProfileCounts';
-import React from 'react';
 
 const Public = () => {
-    const { data, isLoading } = useGetTotalUsersCountQuery()
-    const { data: posts, isLoading:isLoadingPost } = useGetAllPublicPostsQuery({})
+    const { data, isLoading, error } = useGetTotalUsersCountQuery()
+    const { data: posts, isLoading:isLoadingPost, error:errorPost } = useGetAllPublicPostsQuery({})
 
     if (isLoading||isLoadingPost) {
         return <div>Loading...</div>
+    }
+    if(error||errorPost){
+        return <div>Error...</div>
     }
 
     return <div>
