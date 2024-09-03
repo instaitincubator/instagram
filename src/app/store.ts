0817ee698ca8edfd5f46e516ab279f965176d0b0
gem4ik <mgemskij@gmail.com>
2024-09-03 15:07:26 +0300

    test

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,16 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 
+import { baseCityApi } from '@/app/cities-api'
 import { googleReducer } from '@/app/googleSlice'
 import { baseApi } from '@/services/inctagram-api'
 import { configureStore } from '@reduxjs/toolkit'
 
 export const store = configureStore({
-  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(baseApi.middleware).concat(baseCityApi.middleware),
   reducer: {
     [baseApi.reducerPath]: baseApi.reducer,
+    [baseCityApi.reducerPath]: baseCityApi.reducer,
     google: googleReducer,
   },
 })
--- src/features/profile-settings-form/profile-settings-form.tsx
+++ src/features/profile-settings-form/profile-settings-form.tsx
@@ -1,18 +1,9 @@
 import { useProfileSettingsForm } from '@/features/profile-settings-form/useProfileSettingsForm'
-import { useGetCitiesQuery } from '@/services/cities/citiesApi'
-import {
-  ControlledDatepicker,
-  ControlledInput,
-  ControlledSelect,
-  ControlledTextarea,
-} from '@/shared/ui'
+import { ControlledDatepicker, ControlledInput, ControlledTextarea } from '@/shared/ui'
 import Button from '@/shared/ui/Button/Button'
 
 export const ProfileSettingsForm = () => {
   const { control } = useProfileSettingsForm()
-  const { data } = useGetCitiesQuery({})
-
-  console.log(data)
 
   return (
     <form className="w-full flex flex-col gap-6 pt-[24px]">
@@ -29,7 +20,6 @@ export const ProfileSettingsForm = () => {
       <div className="flex gap-[24px]">
         <ControlledInput control={control} fullWidth label="Select your country" name="country" />
         <ControlledInput control={control} fullWidth label="Select your city" name="city" />
-        <ControlledSelect name="country" options={[]} />
       </div>
       <ControlledTextarea
         control={control}
