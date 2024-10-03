6bb4420c449af4fe58890a200d36414b960d8b33
ekameyshaaa <ekameyshaaa@gmail.com>
2024-10-03 22:25:59 +0300

    fix: fix select language

--- src/features/language-select/LanguageSelect.tsx
+++ src/features/language-select/LanguageSelect.tsx
@@ -6,15 +6,28 @@ import { Option } from '@/shared/ui/Select/types'
 import { cn } from '@/shared/utils/cn'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
+import { types } from 'sass'
+
+import String = types.String
 
 export const LanguageSelect = () => {
   const router = useRouter()
+  const currentPath = router.asPath
+  const changeLangHandler = async (value: Option | string) => {
+    let locale
 
-  const changeLangHandler = (value: Option | string) => {
-    const locale = value as string
-    const currentPath = router.asPath
+    if (typeof value === 'object' && 'value' in value) {
+      locale = value.value as string
+    } else {
+      locale = value
+    }
 
-    router.replace(currentPath, currentPath, { locale })
+    console.log(locale)
+    try {
+      await router.replace(currentPath, currentPath, { locale })
+    } catch (e) {
+      console.log(e)
+    }
   }
   const isMobile = useIsMobile(480)
 
