350fe56037d6b5a51e70100ba8f2950990033b8e
ekameyshaaa <ekameyshaaa@gmail.com>
2024-10-20 17:57:58 +0300

    fix: redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,28 @@
 import { useEffect } from 'react'
 
 import { getLayout } from '@/app/layouts/mainLayout/Layout'
-import { useGoogleSignInMutation } from '@/services/auth/signInApi'
+import { useGoogleSignInMutation, useMeQuery } from '@/services/auth/signInApi'
 import { useRouter } from 'next/router'
 
 export default function Home() {
   const router = useRouter()
   const { code } = router.query
-  const [googleSignIn] = useGoogleSignInMutation()
+  const [googleSignIn, { isSuccess }] = useGoogleSignInMutation()
+  const { data: me } = useMeQuery()
 
   useEffect(() => {
     if (code !== undefined && code !== '') {
       googleSignIn({ code: code })
     }
-  }, [router, googleSignIn, code])
+  }, [router, googleSignIn, code, me])
+
+  useEffect(() => {
+    if (isSuccess && me) {
+      const userId = me.userId
+
+      router.push(`/profile/${userId}`)
+    }
+  }, [isSuccess, me, router])
 
   return <div className="flex py-[100px] px-[100px]"></div>
 }
