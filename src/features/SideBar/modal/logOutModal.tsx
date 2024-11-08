0e8d752b2c48aabf625c367568935400ad6e2d1a
ekameyshaaa <ekameyshaaa@gmail.com>
2024-11-08 16:51:41 +0300

    create logOut modal and fix redirect logOut

---
+++ src/features/SideBar/modal/logOutModal.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { useTranslation } from '@/shared/hooks/useTranslation'
+import Button from '@/shared/ui/Button/Button'
+import { Modal } from '@/shared/ui/Modal/Modal'
+
+type PropsType = {
+  confirm: () => void
+  email: string
+  onClose: () => void
+}
+const LogOutModal = ({ confirm, email, onClose }: PropsType) => {
+  const { t } = useTranslation()
+
+  return (
+    <Modal onClose={onClose} title={t.auth.logOut}>
+      <div>
+        <h2 className={'w-300 text-regular-16 pt-3.5 pb-[58px] leading-6 md:w-[390px]'}>
+          {t.auth.wantLogOut} &ldquo;<strong className="text-bold-16">{email}</strong>
+          ?&ldquo;
+        </h2>
+        <div className="flex gap-6 mb-5 justify-center md:justify-end">
+          <Button onClick={confirm} variant={'outline'}>
+            {t.generalInformation.yes}
+          </Button>
+          <Button onClick={onClose}>{t.auth.no}</Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default LogOutModal
