14b0f57e85af3f8895371eec5674c889ebcf469d
gem4ik <mgemskij@gmail.com>
2024-11-15 16:12:47 +0300

    fixes for deploy

--- src/pages/api/create-checkout-session.ts
+++ src/pages/api/create-checkout-session.ts
@@ -7,47 +7,7 @@ const stripe = new Stripe(config.stripeKey!)
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
-    // Обработка вебхука
-    if (req.headers['stripe-signature']) {
-      const sig = req.headers['stripe-signature']
-      let event
-
-      try {
-        event = stripe.webhooks.constructEvent(
-          req.body,
-          sig,
-          'whsec_TXl6mt30KQpyYVXv4ZTfhB90LcnekMT6'
-        )
-      } catch (err: any) {
-        console.error('Ошибка при обработке вебхука:', err)
-
-        return res.status(400).send(`Ошибка: ${err.message}`)
-      }
-      console.log(event.type)
-
-      switch (event.type) {
-        case 'payment_intent.succeeded': {
-          const paymentIntentSucceeded = event.data.object
-
-          console.log('Успех:', paymentIntentSucceeded.id)
-          break
-        }
-        case 'payment_intent.payment_failed': {
-          const paymentIntentFailed = event.data.object
-          const message = paymentIntentFailed.last_payment_error?.message
-
-          console.log('Неудача:', paymentIntentFailed.id, message)
-          break
-        }
-        default:
-          console.log(`Необработанное событие: ${event.type}`)
-      }
-
-      return res.status(200).send('Вебхук обработан')
-    }
-
-    // Обработка создания сессии оплаты
-    const { amount, name } = req.body // Сумма в центах
+    const { amount, name } = req.body
 
     try {
       const session = await stripe.checkout.sessions.create({
