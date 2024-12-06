988d764b7a3b2a64b1d647b82f62562b3d6fd57d
gem4ik <mgemskij@gmail.com>
2024-12-06 13:04:47 +0300

    deploy commit

--- src/pages/google/index.tsx
+++ src/pages/google/index.tsx
@@ -1,11 +1,12 @@
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 
 import { getLayoutWithSidebar } from '@/app/layouts/layoutWithSidebar/LayoutWithSidebar'
-import { useGoogleSignInMutation } from '@/services/auth/signInApi'
+import { useGoogleSignInMutation, useMeQuery } from '@/services/auth/signInApi'
 import { useRouter } from 'next/router'
 
 export const Google = () => {
-  const [googleSignIn] = useGoogleSignInMutation()
+  const [googleSignIn, { isSuccess }] = useGoogleSignInMutation()
+  const { data: me } = useMeQuery()
 
   const router = useRouter()
   const { code } = router.query
@@ -15,6 +16,14 @@ export const Google = () => {
       googleSignIn({ code: code })
     }
   }, [code])
+
+  useLayoutEffect(() => {
+    if (isSuccess && me) {
+      const userId = me?.userId
+
+      router.push(`/profile/${userId}`)
+    }
+  }, [isSuccess, me, router])
 
   return <div>google code: {router.query.code}</div>
 }
