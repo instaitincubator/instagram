503b588a66c5b67a3cb68fbfa70fb90d0a648498
gem4ik <mgemskij@gmail.com>
2024-12-06 13:00:21 +0300

    deploy commit

--- src/pages/google/index.tsx
+++ src/pages/google/index.tsx
@@ -1,8 +1,20 @@
+import { useEffect } from 'react'
+
 import { getLayoutWithSidebar } from '@/app/layouts/layoutWithSidebar/LayoutWithSidebar'
+import { useGoogleSignInMutation } from '@/services/auth/signInApi'
 import { useRouter } from 'next/router'
 
 export const Google = () => {
+  const [googleSignIn] = useGoogleSignInMutation()
+
   const router = useRouter()
+  const { code } = router.query
+
+  useEffect(() => {
+    if (code) {
+      googleSignIn({ code: code })
+    }
+  }, [code])
 
   return <div>google code: {router.query.code}</div>
 }
