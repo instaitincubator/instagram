4a74f1b9fbbddc2d57676301417c3ce2928661e7
hardmass <hardmass3ffect@gmail.com>
2024-12-10 15:15:31 +0300

    fis imports in userAvatar components

--- src/entities/UserAvatar/UserAvatar.tsx
+++ src/entities/UserAvatar/UserAvatar.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import button from '@/shared/ui/Button/Button'
 import Button from '@/shared/ui/Button/Button'
 import Image from 'next/image'
 
@@ -29,9 +28,9 @@ const UserAvatar = ({ avatar, isShowedText, toggleShowedText, userId, userName }
         <p>{userName}</p>
       </a>
       {isShowedText && (
-        <button className="cursor-pointer" onClick={toggleShowedText}>
+        <Button className="cursor-pointer" onClick={toggleShowedText}>
           <Block />
-        </button>
+        </Button>
       )}
     </div>
   )
