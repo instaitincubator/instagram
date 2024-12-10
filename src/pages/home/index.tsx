963440ffd5a7acdff434d56ff20e367e1cd840d0
gem4ik <mgemskij@gmail.com>
2024-12-10 14:12:05 +0300

    for deploy

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { getPublicLayoutWithSidebar } from '@/app/layouts/PublicLayoutWithSidebar/PublicLayoutWithSidebar'
 
 export default function Home() {
-  return <div>home </div>
+  return <div> home </div>
 }
 
 Home.getLayout = getPublicLayoutWithSidebar
