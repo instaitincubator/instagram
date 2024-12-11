ff37efd869f7f5474d3a6401a4e15f8079f9cc65
hardmass <hardmass3ffect@gmail.com>
2024-12-11 19:05:43 +0300

    add table for my payments

--- src/pages/profile/settings/my-payments/index.tsx
+++ src/pages/profile/settings/my-payments/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
 
 import { getSettingsLayout } from '@/app/layouts/settingsLayout/SettingsLayout'
+import { useTableConfig } from '@/pages/profile/settings/my-payments/useTableConfig'
+import { useGetPaymentsQuery } from '@/services/payments/PaymentsApi'
+import { Table } from '@/shared/ui/Table/TableRoot'
 
 const MyPayments = () => {
-  return <div>MyPayments</div>
+  const { data: payments } = useGetPaymentsQuery()
+  const { columns, formatPrice, setSort, sort } = useTableConfig()
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="w-full overflow-x-auto">
+        <Table.TableRoot>
+          <Table.TableHeader columns={columns} onSort={setSort} sort={sort} />
+          <Table.TableBody>
+            {payments?.map((payment, index) => (
+              <Table.TableRow key={index}>
+                <Table.TableCell>
+                  {new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}
+                </Table.TableCell>
+                <Table.TableCell>
+                  {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
+                </Table.TableCell>
+                <Table.TableCell>{formatPrice(payment.price)}</Table.TableCell>
+                <Table.TableCell>{payment.subscriptionType}</Table.TableCell>
+                <Table.TableCell>{payment.paymentType}</Table.TableCell>
+              </Table.TableRow>
+            ))}
+          </Table.TableBody>
+        </Table.TableRoot>
+      </div>
+      <div>{/*<Pagination currentPage={10} pageSize={10} totalCount={20} />*/}</div>
+    </div>
+  )
 }
 
 MyPayments.getLayout = getSettingsLayout
---
+++ src/pages/profile/settings/my-payments/useTableConfig.ts
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+import { Sort } from '@/shared/ui/Table/types'
+
+export const useTableConfig = () => {
+  const [sort, setSort] = useState<Sort>({ direction: 'asc', key: 'cardsCount' })
+  const columns = [
+    {
+      key: 'Date of Payment',
+      sortable: true,
+      title: 'Date of Payment',
+    },
+    {
+      key: 'End date of subscription',
+      sortable: true,
+      title: 'End date of subscription',
+    },
+    {
+      key: 'Price',
+      sortable: true,
+      title: 'Price',
+    },
+    {
+      key: 'Subscription Type',
+      sortable: true,
+      title: 'Subscription Type',
+    },
+    {
+      key: 'Payment Type',
+      sortable: false,
+      title: 'Payment Type',
+    },
+  ]
+
+  const formatPrice = (price: number) => {
+    switch (price) {
+      case 10000:
+        return '$100'
+      case 5000:
+        return '$50'
+      case 1000:
+        return '$10'
+      default:
+        return `$${(price / 100).toLocaleString(undefined, { maximumFractionDigits: 2, minimumFractionDigits: 0 })}`
+    }
+  }
+
+  return { columns, formatPrice, setSort, sort }
+}
