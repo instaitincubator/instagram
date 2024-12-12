6b6944e3ed3f99980a60225451798d44f69aa4c1
hardmass <hardmass3ffect@gmail.com>
2024-12-12 22:17:34 +0300

    refactor: check MyPaymentsTable component

--- src/pages/profile/settings/my-payments/index.tsx
+++ src/pages/profile/settings/my-payments/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 import { getSettingsLayout } from '@/app/layouts/settingsLayout/SettingsLayout'
+import { MyPaymentsTable } from '@/pages/profile/settings/my-payments/myPaymentsTable'
 import { useGetPaymentsQuery } from '@/services/payments/PaymentsApi'
 import { useTableConfig } from '@/shared/hooks/useTableConfig'
-import { Table } from '@/shared/ui/Table/TableRoot'
 
 const MyPayments = () => {
   const { data: payments } = useGetPaymentsQuery()
@@ -11,26 +11,13 @@ const MyPayments = () => {
 
   return (
     <div className="flex flex-col gap-8">
-      <div className="w-full overflow-x-auto">
-        <Table.TableRoot>
-          <Table.TableHeader columns={columns} onSort={setSort} sort={sort} />
-          <Table.TableBody>
-            {payments?.map((payment, index) => (
-              <Table.TableRow key={index}>
-                <Table.TableCell>
-                  {new Date(payment.dateOfPayment).toLocaleDateString('ru-RU')}
-                </Table.TableCell>
-                <Table.TableCell>
-                  {new Date(payment.endDateOfSubscription).toLocaleDateString('ru-RU')}
-                </Table.TableCell>
-                <Table.TableCell>{formatPrice(payment.price)}</Table.TableCell>
-                <Table.TableCell>{payment.subscriptionType}</Table.TableCell>
-                <Table.TableCell>{payment.paymentType}</Table.TableCell>
-              </Table.TableRow>
-            ))}
-          </Table.TableBody>
-        </Table.TableRoot>
-      </div>
+      <MyPaymentsTable
+        columns={columns}
+        formatPrice={formatPrice}
+        payments={payments}
+        setSort={setSort}
+        sort={sort}
+      />
       <div>{/*<Pagination currentPage={10} pageSize={10} totalCount={20} />*/}</div>
     </div>
   )
