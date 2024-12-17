15b10456196d40a9a2d31656cd0d6a1c9cc8a2d5
gem4ik <mgemskij@gmail.com>
2024-12-17 16:38:34 +0300

    deploy commit

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,30 +1,20 @@
-import React, { useLayoutEffect, useState } from 'react'
+import React, { useLayoutEffect } from 'react'
 
 import { getLayoutWithSidebar } from '@/app/layouts/layoutWithSidebar/LayoutWithSidebar'
-import { useGoogleSignInMutation, useMeQuery } from '@/services/auth/signInApi'
-import { useGetProfileInfoQuery } from '@/services/profile/profileApi'
-import { GetProfilePostsParams } from '@/shared/types/ApiTypes/ProfileApiTypes'
+import { useMeQuery } from '@/services/auth/signInApi'
 import { useRouter } from 'next/router'
 
 const Profile = () => {
-  const { data: me, isLoading: isMeLoading } = useMeQuery()
-  const [googleSignIn, { isLoading: isSignInLoading, isSuccess }] = useGoogleSignInMutation()
+  const { data: me } = useMeQuery()
   const router = useRouter()
 
   useLayoutEffect(() => {
-    if (isSuccess || me) {
+    if (me) {
       const userId = me?.userId
 
-      router.push(`/profile/${userId}`)
+      void router.push(`/profile/${userId}`)
     }
-  }, [isSuccess, me, router])
-  const params: GetProfilePostsParams = {
-    userName: me?.userName!,
-  }
-
-  if (isSignInLoading || isMeLoading) {
-    return <div>Loading...</div>
-  }
+  }, [me, router])
 
   return (
     <div className="flex flex-col gap-[13px] flex-1 pt-[24px] px-[15px] md:pr-16 md:pl-6 md:pt-[35px] w-full"></div>
