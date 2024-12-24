29ff0eab828acdcef46ff3b4d99d83fd4b9ed834
ekameyshaaa <ekameyshaaa@gmail.com>
2024-12-24 20:33:24 +0300

    fix: public post

--- src/features/public/PublicPosts.tsx
+++ src/features/public/PublicPosts.tsx
@@ -17,7 +17,7 @@ const PublicPosts = ({ posts }: Props) => {
 
   return (
     <div
-      className="md:grid md:grid-cols-posts py-[36px] flex justify-center items-center flex-col md:justify-between mx-auto flex-wrap flex-grow gap-[12px]"
+      className="grid grid-cols-posts py-[36px] justify-between mx-auto flex-wrap flex-grow gap-[12px]"
     >
       {posts?.map(post => (
         <PostCard key={post.id} openModal={() => openModal(post)} post={post} />
