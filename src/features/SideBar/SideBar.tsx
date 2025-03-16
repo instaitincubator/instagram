15fee3ca3cb5273ab5cbf9c9f18b12d569b4e5f3
gem4ik <mgemskij@gmail.com>
2025-03-16 14:50:15 +0300

    fix query adding

--- src/features/SideBar/SideBar.tsx
+++ src/features/SideBar/SideBar.tsx
@@ -77,7 +77,6 @@ export const SideBar = () => {
           child1={<Plus />}
           child2={<PlusFill />}
           className="order-2"
-          // href={'/create-post'}
           setActiveLink={setActiveLink}
           title={t.sidebar.create}
         ></CustomLink>
