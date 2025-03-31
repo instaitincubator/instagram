475c09b5aa56bf1b63bbedfece85e6fa2b8c2de9
Alexandr1991Sh <an1991sh@yandex.ru>
2025-03-31 19:16:09 +0300

    unsubscribe from user from post menu

--- src/features/home/ui/MobilePostMenu.tsx
+++ src/features/home/ui/MobilePostMenu.tsx
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
+import {
+  useFollowingUserMutation,
+  useGetUserWithFollowingStatusQuery,
+  useUnFollowingUserMutation,
+} from '@/services/users/users-following/usersFollowing-api'
 import { CopyLink } from '@/shared/ui/icons/copyLink'
 import { Follow } from '@/shared/ui/icons/follow'
 import { Menu } from '@/shared/ui/icons/menu'
 import { UnFollow } from '@/shared/ui/icons/unFollow'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
-type MobilePostMenuProps = {
+interface Props {
   imageUrl: string
+  userId: number
 }
 
-export const MobilePostMenu = ({ imageUrl }: MobilePostMenuProps) => {
+export const MobilePostMenu = ({ imageUrl, userId }: Props) => {
+  const [unFollow, { isSuccess }] = useUnFollowingUserMutation()
+
+  const unfollowUser = () => {
+    unFollow(userId)
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      window.location.reload()
+    }
+  }, [isSuccess])
+
   const copyLinkHandler = () => {
     navigator.clipboard.writeText(imageUrl)
   }
@@ -23,9 +41,10 @@ export const MobilePostMenu = ({ imageUrl }: MobilePostMenuProps) => {
       <DropdownMenu.Portal>
         <DropdownMenu.Content className="text-light-100 mr-4 text-regular-14 bg-dark-500 w-fit rounded-sm border border-dark-100 px-2 py-3">
           <DropdownMenu.Item className="group leading-none flex items-center h-9  relative select-none outline-none cursor-pointer">
-            <Follow />
             <UnFollow />
-            <span className="ml-[7px]">UnFollow</span>
+            <span className="ml-[7px]" onClick={unfollowUser}>
+              UnFollow
+            </span>
           </DropdownMenu.Item>
           <DropdownMenu.Item
             className="group leading-none flex items-center h-9 relative select-none outline-none cursor-pointer"
