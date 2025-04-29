48adb658301076a49ddc3b621660b65c8dd11ed4
Alexandr1991Sh <an1991sh@yandex.ru>
2025-04-29 21:47:38 +0300

    fixed Messenger component

--- src/features/messanger/Messanger.tsx
+++ src/features/messanger/Messanger.tsx
@@ -7,23 +7,20 @@ import { ChooseChatPanel } from './ui/ChooseChatPanel/ChooseChatPanel'
 
 export const Messenger = () => {
   const [selectedChat, setSelectedChat] = useState(null)
-
   const isMobile = useMediaQuery({ query: '(max-width: 768px)' })
+
+  const showChooseChatPanel = selectedChat === null || !isMobile
+  const showMessageBlock = isMobile ? selectedChat !== null : true
 
   return (
     <div className="h-full flex flex-col sm:flex-row">
-      {isMobile ? (
-        <>{selectedChat ? <MessageBlock /> : <ChooseChatPanel />}</>
-      ) : (
-        <>
-          <div className="hidden sm:flex sm:max-w-[300px]">
-            <ChooseChatPanel />
-          </div>
-          <div className="hidden flex-1 sm:flex">
-            <MessageBlock />
-          </div>
-        </>
-      )}
+      <div className={`${isMobile ? 'flex-1' : 'flex'} ${showChooseChatPanel ? 'flex' : 'hidden'}`}>
+        <ChooseChatPanel />
+      </div>
+
+      <div className={`flex-1 ${showMessageBlock ? 'flex' : 'hidden'}`}>
+        <MessageBlock />
+      </div>
     </div>
   )
 }
