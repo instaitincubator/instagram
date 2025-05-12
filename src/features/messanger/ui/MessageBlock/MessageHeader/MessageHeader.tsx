ab9526d921e546e156f1a155b67198eae6d2a2d3
Alexandr1991Sh <an1991sh@yandex.ru>
2025-05-12 20:17:16 +0300

    fix Image

--- src/features/messanger/ui/MessageBlock/MessageHeader/MessageHeader.tsx
+++ src/features/messanger/ui/MessageBlock/MessageHeader/MessageHeader.tsx
@@ -19,6 +19,8 @@ export const MessageHeader = ({ isMobile, userAvatar, userName }: Props) => (
     <p className={'pl-4 ml-[25px] md:hidden text-regular-16'}>{userName}</p>
     {userName ? (
       <Image
+        width={48}
+        height={48}
         alt={'avatar'}
         className={
           isMobile ? 'rounded-full w-[36px] h-[36px] ml-auto' : 'rounded-full w-[48px] h-[48px]'
